a2e1f988d0e643b6fcacdd3b3075327a9779dd56
brtkrclr <berat.karacalar@optiim.com>
2021-05-31 00:36:54 +0300

    activites added

--- app/src/main/java/com/example/remotion/MainActivity.java
+++ app/src/main/java/com/example/remotion/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.remotion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button learnButton=findViewById(R.id.learnButton);
+        Button playButton=findViewById(R.id.learnButton);
+        Button settingsButton=findViewById(R.id.learnButton);
+
+        learnButton.setOnClickListener(v -> {
+            Intent intent=new Intent(this,LearnActivity.class);
+            startActivity(intent);
+        });
+        playButton.setOnClickListener(v -> {
+            Intent intent=new Intent(this,PlayActivity.class);
+            startActivity(intent);
+        });
+        settingsButton.setOnClickListener(v -> {
+            Intent intent=new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+        });
     }
 }
