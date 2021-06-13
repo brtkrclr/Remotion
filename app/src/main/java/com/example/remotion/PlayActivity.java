ee735739265fb6d81be76ad33f06b4d4124d5209
brtkrclr <berat.karacalar@optiim.com>
2021-06-13 23:49:49 +0300

    retrieve img playpart

--- app/src/main/java/com/example/remotion/PlayActivity.java
+++ app/src/main/java/com/example/remotion/PlayActivity.java
@@ -1,19 +1,55 @@
 package com.example.remotion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
 
 public class PlayActivity extends AppCompatActivity {
+    ImageView imageView;
+    private FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference=firebaseDatabase.getReference();
+    private DatabaseReference myRef=databaseReference.child("image");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        imageView=(ImageView)findViewById(R.id.imagePlay);
+
 
         View view=getWindow().getDecorView();
         view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String link=snapshot.getValue(String.class);
+                Picasso.get().load(link).into(imageView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 }
